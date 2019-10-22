105f4f2dd7945e8d89c0c19473d56b84e04a5063
Liam Deppong <liamdepp0605@gmail.com>
2019-10-22 16:01:22 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+/*----------Random Damage Arrow Function----------*/
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10 + 1);
+
+}
+
+
+/*----------Choose Option Arrow Function----------*/
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.floor(Math.random() * 2);
+    return randNum === 0 ? opt1 : opt2;
+}
+
+
+/*----------Attack Player Expression Function----------*/
+const attackPlayer = function(health) {
+    return health -= randomDamage();
+}
+
+
+/*----------Log Health Arrow Function----------*/
+const logHealth = (player, health) =>{
+    console.log(`${player} health: ${health}`);
+}
+
+
+/*----------Log Death Arrow Function----------*/
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`);
+}
+
+
+/*----------Is Dead Arrow Function----------*/
+const isDead = (health) =>{
+    if (health <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/*----------Fight Declaration Function----------*/
+function fight(player1, player2, player1Health, player2Health){
+    while(true){
+        let attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if(isDead(player2Health)){
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if(isDead(player1Health)){
+                logDeath(player2, player1);
+                break;
+            }
+        }
+    }
+}
+fight("Liam", "Abbie", 100, 100);
